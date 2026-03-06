a03b1ec6177faf429f0b6f56fad3e57c7c37d3ec
sirthus <texasbeat@gmail.com>
2026-03-05 19:48:11 -0600

    Add foreign key indexing script

---
+++ sql/008_foreign_key_indexes.sql
@@ -0,0 +1,8 @@
+CREATE INDEX accounts_customer_id_idx
+    ON bank.accounts (customer_id);
+
+CREATE INDEX transactions_account_id_idx
+    ON bank.transactions (account_id);
+
+CREATE INDEX transactions_merchant_id_idx
+    ON bank.transactions (merchant_id);
