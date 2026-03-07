a35d130532f2d01db96ade013057de1d14874e18
sirthus <texasbeat@gmail.com>
2026-03-06 18:00:29 -0600

    Add schema reset script

---
+++ scripts/reset-buildtest.ps1
@@ -0,0 +1,4 @@
+docker exec pg18 psql -U postgres -d postgres -c "DROP DATABASE IF EXISTS modernize_buildtest;"
+docker exec pg18 psql -U postgres -d postgres -c "CREATE DATABASE modernize_buildtest;"
+
+.\scripts\build-schema.ps1
