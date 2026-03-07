774bb8c6741e72b1a4cc4fc2ad25a693e0ad96d5
sirthus <texasbeat@gmail.com>
2026-03-06 19:19:23 -0600

    Add verify script and trigger from reset script

--- scripts/reset-buildtest.ps1
+++ scripts/reset-buildtest.ps1
@@ -2,3 +2,4 @@ docker exec pg18 psql -U postgres -d postgres -c "DROP DATABASE IF EXISTS modern
 docker exec pg18 psql -U postgres -d postgres -c "CREATE DATABASE modernize_buildtest;"
 
 .\scripts\build-schema.ps1
+.\scripts\verify-buildtest.ps1
